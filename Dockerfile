3c48b3758ac47592deec9b27a275825ebba21d64
Lorenzo Scalese <16834099+lscalese@users.noreply.github.com>
2022-03-16 17:18:29 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,44 @@
+ARG IMAGE=containers.intersystems.com/intersystems/iris:2021.1.0.215.0
+# currently there is an issue with 2021.2.0.617.0, work in progress...
+# ARG IMAGE=containers.intersystems.com/intersystems/iris:2021.2.0.617.0
+
+FROM $IMAGE
+
+USER root
+
+COPY session.sh /
+COPY iris.key /usr/irissys/mgr/iris.key
+
+# Install iputils-arping to have arping command.  It's required to configure Virtual IP.
+# Download the latest ZPM version...
+RUN mkdir /opt/demo && \
+    chown ${ISC_PACKAGE_MGRUSER}:${ISC_PACKAGE_IRISGROUP} /opt/demo && \
+    chmod 666 /usr/irissys/mgr/iris.key && \
+    apt-get update && apt-get install iputils-arping && \
+    wget -O /opt/demo/zpm.xml https://pm.community.intersystems.com/packages/zpm/latest/installer
+
+USER ${ISC_PACKAGE_MGRUSER}
+
+WORKDIR /opt/demo
+
+# Set Default Miror role to master, will be override on docker-compose file.
+ARG IRIS_MIRROR_ROLE=master
+
+ADD config-files .
+
+SHELL [ "/session.sh" ]
+
+# Install ZPM, config-api and pki-script
+# Load a simple configuration file in json config-api format to : 
+#  - create "myappdata" database.
+#  - add a global mapping in namespace "USER" for global "demo.*" on "myappdata" database.
+RUN \
+Do $SYSTEM.OBJ.Load("/opt/demo/zpm.xml", "ck") \
+zpm "install config-api" \
+zpm "install pki-script" \
+Set sc = ##class(Api.Config.Services.Loader).Load("/opt/demo/simple-config.json")
+
+COPY init_mirror.sh /
+
+# Execute a post start script to configure mirroring.
+CMD ["-a", "/init_mirror.sh"]
---
+++ init_mirror.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# Database used to test the mirror.
+DATABASE=/usr/irissys/mgr/myappdata
+
+# Directory contain myappdata backuped by the master to restore on other nodes and making mirror.
+BACKUP_FOLDER=/opt/backup
+
+# Mirror configuration file in json config-api format for the master node.
+MASTER_CONFIG=/opt/demo/mirror-master.json
+
+# Mirror configuration file in json config-api format for the backup node.
+BACKUP_CONFIG=/opt/demo/mirror-backup.json
+
+# Mirror configuration file in json config-api format for the report async node.
+REPORT_CONFIG=/opt/demo/mirror-report.json
+
+# The mirror name...
+MIRROR_NAME=DEMO
+
+# Mirror Member list.
+MIRROR_MEMBERS=BACKUP,REPORT
+
+# PKI Server host:port (PKI Server is installed on master instance)
+PKISERVER=master:52773
+
+# Performed on the master.
+# Configure Public Key Infrastructure Server on this instance and generate certificate in order to configure a mirror using SSL.
+#   See article https://community.intersystems.com/post/creating-ssl-enabled-mirror-intersystems-iris-using-public-key-infrastructure-pki
+#   and the related tools https://openexchange.intersystems.com/package/PKI-Script
+# Load the mirror configuration using config-api with /opt/demo/simple-config.json file.
+# Start a Job to auto-accept other members named "backup" and "report" to join the mirror (avoid manuel validation in portal management).
+master() {
+rm -rf $BACKUP_FOLDER/IRIS.DAT
+iris session $ISC_PACKAGE_INSTANCENAME -U %SYS <<- END
+Do ##class(lscalese.pki.Utils).MirrorMaster(,"",,,,"${MIRROR_MEMBERS}")
+Set sc = ##class(Api.Config.Services.Loader).Load("${MASTER_CONFIG}")
+Set ^log.mirrorconfig(\$i(^log.mirrorconfig)) = \$SYSTEM.Status.GetOneErrorText(sc)
+Job ##class(Api.Config.Services.SYS.MirrorMaster).AuthorizeNewMembers("${MIRROR_MEMBERS}","${MIRROR_NAME}",600)
+Hang 2
+Halt
+END
+}
+
+# Performed by the master, make a backup of /usr/irissy
+make_backup() {
+iris session $ISC_PACKAGE_INSTANCENAME -U %SYS "##class(SYS.Database).DismountDatabase(\"${DATABASE}\")"
+md5sum ${DATABASE}/IRIS.DAT
+cp ${DATABASE}/IRIS.DAT ${BACKUP_FOLDER}/IRIS.TMP
+mv ${BACKUP_FOLDER}/IRIS.TMP ${BACKUP_FOLDER}/IRIS.DAT
+chmod 777 ${BACKUP_FOLDER}/IRIS.DAT
+iris session $ISC_PACKAGE_INSTANCENAME -U %SYS "##class(SYS.Database).MountDatabase(\"${DATABASE}\")"
+}
+
+# Restore the mirrored database "myappdata".  This restore is performed on "backup" and "report" node.
+restore_backup() {
+sleep 5
+while [ ! -f $BACKUP_FOLDER/IRIS.DAT ]; do sleep 1; done
+sleep 2
+iris session $ISC_PACKAGE_INSTANCENAME -U %SYS "##class(SYS.Database).DismountDatabase(\"${DATABASE}\")"
+cp $BACKUP_FOLDER/IRIS.DAT $DATABASE/IRIS.DAT
+md5sum $DATABASE/IRIS.DAT
+iris session $ISC_PACKAGE_INSTANCENAME -U %SYS "##class(SYS.Database).MountDatabase(\"${DATABASE}\")"
+}
+
+# Configure the "backup" member
+#  - Configure Public Key Infrastructure client to install certificate and use SSL with the mirror.
+#  - Load configuration file /opt/demo/mirror-backup.json if this instance is the backup or 
+#    /opt/demo/mirror-report.json if this instance the report (async R\W mirror node).
+other_node() {
+sleep 5
+iris session $ISC_PACKAGE_INSTANCENAME -U %SYS <<- END
+Do ##class(lscalese.pki.Utils).MirrorBackup("${PKISERVER}","")
+Set sc = ##class(Api.Config.Services.Loader).Load("$1")
+Halt
+END
+}
+
+if [ "$IRIS_MIRROR_ROLE" == "master" ]
+then
+  master
+  make_backup
+elif [ "$IRIS_MIRROR_ROLE" == "backup" ]
+then
+  restore_backup
+  other_node $BACKUP_CONFIG
+else
+  restore_backup
+  other_node $REPORT_CONFIG
+fi
+
+exit 0
